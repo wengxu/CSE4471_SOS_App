65723d825d62695afaf35e281853ec26cd4777e9
xiangyu zhang <a842100330@gmail.com>
2016-03-22 23:21:43 -0400

    add GPS

--- SOS_OSU/app/src/main/java/com/cse4471/osu/sos_osu/MainActivity.java
+++ SOS_OSU/app/src/main/java/com/cse4471/osu/sos_osu/MainActivity.java
@@ -1,24 +1,74 @@
 package com.cse4471.osu.sos_osu;
 
+
+import android.Manifest;
+import android.content.Context;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtLat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // Should we show an explanation?
+            ActivityCompat.requestPermissions(this,new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
+            return;
+        }
+
+        txtLat = (TextView) findViewById(R.id.textview1);
+        txtLat.setText("Latitude:" + ", Longitude:");
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, locationListener);
+
+
         final Button sos= (Button)findViewById(R.id.button);
-        final CountDownTimer timer= new CountDownTimer(5000, 1000) {
+        final CountDownTimer timer= new CountDownTimer(5999, 1000) {
             public void onTick(long millisUntilFinished) {
                 sos.setText("" + (millisUntilFinished / 1000));
             }
@@ -30,14 +80,14 @@ public class MainActivity extends AppCompatActivity {
 
         sos.setTag(1);
         sos.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void  onClick(View v){
-                        final int status =(Integer) v.getTag();
-                        if (status!=1){
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        final int status = (Integer) v.getTag();
+                        if (status != 1) {
                             sos.setText("sos");
                             sos.setTag(1);
                             timer.cancel();
-                        }else{
+                        } else {
                             sos.setTag(0);
                             timer.start();
                         }
@@ -48,12 +98,14 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
